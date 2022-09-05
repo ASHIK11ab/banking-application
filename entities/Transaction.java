181bc82f5cf034c25c51b9f7d1f557bc77eb9070
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2022-09-05 13:16:43 +0530

    added attributes

--- entities/Transaction.java
+++ entities/Transaction.java
@@ -11,8 +11,10 @@ public class Transaction {
     private static long _counter = 100000000000000L;
     public final long id;
     public final String payerAccountNo;
+    public final String payerName;
     public final float payerBeforeBalance;
     public final String payeeAccountNo;
+    public final String payeeName;
     public final float payeeBeforeBalance;
     public final float amount;
     public final boolean isSuccessfull;
@@ -24,8 +26,10 @@ public class Transaction {
         Transaction._counter++;
         this.id = Transaction._counter;
         this.payerAccountNo = payerAccount.getAccountNo();
+        this.payerName = Bank.getCustomer(payerAccount.getCustomerId()).getName();
         this.payerBeforeBalance = payerAccount.getBalance();
         this.payeeAccountNo = payeeAccount.getAccountNo();
+        this.payeeName = Bank.getCustomer(payeeAccount.getCustomerId()).getName();
         this.payeeBeforeBalance = payeeAccount.getBalance();
         this.isSuccessfull = isSuccessfull;
         this.amount = amount;
